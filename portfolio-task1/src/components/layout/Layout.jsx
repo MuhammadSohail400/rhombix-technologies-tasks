e4875d524923509c319c30846c0c8f0602285a7e
Muhammad Sohail <msohailg211@gmail.com>
2026-06-06 18:28:38 +0500

    feat: add scroll progress bar

--- portfolio-task1/src/components/layout/Layout.jsx
+++ portfolio-task1/src/components/layout/Layout.jsx
@@ -1,11 +1,14 @@
 import Navbar from "./Navbar"
 import Footer from "./Footer"
 import ParticleBackground from "../ui/ParticleBackground"
+
 import CustomCursor from "../ui/CustomCursor"
+import ScrollProgressBar from "../ui/ScrollProgessBar"
 
 const Layout = ({ children }) => {
   return (
     <div className="bg-bg min-h-screen relative">
+       <ScrollProgressBar />
 
       {/* Global — har jagah dikhega */}
       <ParticleBackground />
---
+++ portfolio-task1/src/components/ui/ScrollProgessBar.jsx
@@ -0,0 +1,23 @@
+
+import { motion, useScroll, useSpring } from "framer-motion"
+
+const ScrollProgressBar = () => {
+
+  const { scrollYProgress } = useScroll()
+
+  // Spring — smooth feel
+  const scaleX = useSpring(scrollYProgress, {
+    stiffness: 100,
+    damping: 30,
+    restDelta: 0.001,
+  })
+
+  return (
+    <motion.div
+      className="fixed top-0 left-0 right-0 z-[99999] h-[2px] origin-left bg-gradient-to-r from-cyan via-accent to-cyan"
+      style={{ scaleX }}
+    />
+  )
+}
+
+export default ScrollProgressBar
